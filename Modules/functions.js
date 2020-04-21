dd084d25a44e969e3cfc18137c9d04d690241238
jensengbg-Farah-Mahboob <45151545+farahm20@users.noreply.github.com>
2020-04-21 11:02:20 +0200

    Modules now working...

--- Modules/functions.js
+++ Modules/functions.js
@@ -1,71 +1,83 @@
-module.exports = ( server, database ) => 
-{    
-    function getProducts(){
-        return database.get('Products').value();
-    }
-    function getCartItems(){
-        return database.get('Cart').value();
-    }
+const low = require ('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
 
-    function addItemInCart(searchTerm){
-        let itemtoAdd = parseInt(searchTerm);
-        //console.log(itemtoAdd);
+const adapter = new FileSync('database.json');
+const database = low(adapter);
 
-    /* You should receive an error message if you try to add a product that does not exist.*/
-        const matchProduct = database.get('Products').find({ id: itemtoAdd }).value();
-        //console.log(matchProduct);
-        if(matchProduct === undefined){
-            alertMessage = {
-                status : 'ERROR', message : "Invalid product ID. Enter correct ID. " };
-                console.log(alertMessage);
-        }else{
-            alertMessage = {
-                status : 'SUCCESS', message : "Valid ID. Item found in Products. "
-            }
+//app.use(express.static('public'));
+const express = require ('express');
+const app = express();
+app.use(express.json())
+
+//-------------------------------------------GET ALL PRODUCTS------------------------------------------- */
+exports.getProducts = () => {
+    return database.get('Products').value();
+}
+
+//-------------------------------------------GET ALL CART ITEMS------------------------------------------- */
+exports.getCartItems = () => {
+    return database.get('Cart').value();
+}
+
+//-------------------------------------------FIND ITEM IN CART------------------------------------------- */
+//This function is only used in the functions.js. So no need to export it.
+function findItemInCart(itemToFind){
+    const findInCart = database.get('Cart').find({ id: itemToFind}).value();
+    //console.log(findInCart);
+    return findInCart;
+}
+
+//-------------------------------------------ADD ITEM TO CART------------------------------------------- */
+exports.addItemInCart = (searchTerm) => {
+    let itemtoAdd = parseInt(searchTerm);
+    //console.log(itemtoAdd);
+
+    /* DISPLAY: error message if you try to add a product that does not exist.*/
+    const matchProduct = database.get('Products').find({ id: itemtoAdd }).value();
+    //console.log(matchProduct);
+    if(matchProduct === undefined){
+        alertMessage = {
+            status : 'ERROR', message : "Invalid product ID. Enter correct ID. " };
             console.log(alertMessage);
+    }else{
+        alertMessage = {
+            status : 'SUCCESS', message : "Valid ID. Item found in Products. "
+        }
+        console.log(alertMessage);
 
-            /*You should not be able to add the same product to the shopping cart again.*/   
-        
-        const checkInCartItem = findItemInCart(itemtoAdd);//find item in cart
-        
-        if(checkInCartItem !== undefined){
-            alertMessage = { 
-                status : 'ERROR', message : "Item already present in Cart" };
-                console.log(alertMessage);
-        }else{
-            database.get('Cart').push(matchProduct).write();
-            
-            alertMessage = {
-                status : 'SUCCESS', message : "Item added in the Cart"};
-                console.log(alertMessage);
-            }
-        }    
-    }
-
-    //You should receive an error message if you try to remove a product that does not exist. 
-
-    function deleteItemFromCart(itemToDelete) {
-        let removeItem= parseInt(itemToDelete);
-        //console.log(removeItem);
-
-        const checkInCartItem = findItemInCart(removeItem);//find item in cart
-        //console.log(checkInCartItem);
-
-        if(checkInCartItem !== undefined){
-            database.get('Cart').remove(checkInCartItem).write();
-            alertMessage = {
-                status : 'SUCCESS', message : "Item removed from Cart" };
-                console.log(alertMessage);
-        }else{
-            alertMessage = {
-                status : 'ERROR', message : "Invalid request- Item not found in cart" };
+    /* NOT ADD THE SAME PRODUCT TO CART CHECK */     
+    const checkInCartItem = findItemInCart(itemtoAdd);//find item in cart
+    
+    if(checkInCartItem !== undefined){
+        alertMessage = { 
+            status : 'ERROR', message : "Item already present in Cart" };
             console.log(alertMessage);
-        }    
-    }
+    }else{
+        database.get('Cart').push(matchProduct).write();
+        
+        alertMessage = {
+            status : 'SUCCESS', message : "Item added in the Cart"};
+            console.log(alertMessage);
+        }
+    }    
+}
+ 
+//-------------------------------------------DELETE ITEM FROM CART------------------------------------------- */
+exports.deleteItemFromCart = (itemToDelete) => {
+    let removeItem= parseInt(itemToDelete);
+    //console.log(removeItem);
 
-    function findItemInCart(itemToFind){
-        const findInCart = database.get('Cart').find({ id: itemToFind}).value();
-        //console.log(findInCart);
-        return findInCart;
-    }
+    const checkInCartItem = findItemInCart(removeItem);//find item in cart
+    //console.log(checkInCartItem);
+
+    if(checkInCartItem !== undefined){
+        database.get('Cart').remove(checkInCartItem).write();
+        alertMessage = {
+            status : 'SUCCESS', message : "Item removed from Cart" };
+            console.log(alertMessage);
+    }else{
+        alertMessage = {
+            status : 'ERROR', message : "Invalid request- Item not found in cart" };
+        console.log(alertMessage);
+    }    
 }
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const low = require ('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 
@@ -9,63 +8,68 @@ const database = low(adapter);
 const express = require ('express');
 const app = express();
 app.use(express.json())
-const functions = require('./Modules/functions.js');
+
+//-------------------------------------------IMPORTING FUNCTIONS MODULE------------------------------------------- */
+const func= require('./Modules/functions.js');
+
+//-------------------------------------------INITIATE DATABASE------------------------------------------- */
 
 initDatabase();
 function initDatabase() {
     database.defaults({ Products: [], Cart: [] }).write();
 }
 
-functions(server, database);
 
-//**********************END POINTS */
-//Be able to add products to a shopping cart.
+//----------------END POINTS-------------------END POINTS--------------------END POINTS---------------- */
+
+
+//--------------------------------------------POST: add a specific product ------------------------------------------- */
 //http://localhost:5000/TechShop/add/?id=1
 app.post('/TechShop/add', (req, res) => {
     const searchTerm = req.query.id;
     //console.log(req.url);
     //console.log(searchTerm);
-    res.send('In add item to cart endpoint');
-    addItemInCart(searchTerm);
+    res.send('In add item to cart endpoint....');
+    func.addItemInCart(searchTerm);
 })
 
+//-------------------------------------------DELETE: remove a specific product ------------------------------------------- */
 //http://localhost:5000/TechShop/removeItem/?id=1
 app.delete('/TechShop/removeItem', (req, res) => {
     const itemToDelete = req.query.id;
     //console.log(req.url);
     //console.log(itemToDelete);
-    res.send('At item to delete from cart endpoint');
-    deleteItemFromCart(itemToDelete);
+    res.send('At item to delete from cart endpoint....');
+    func.deleteItemFromCart(itemToDelete);
 })
 
-
+//-------------------------------------------DELETE: delete teh whole cart ------------------------------------------- */
 //http://localhost:5000/TechShop/deleteAllCart
-//Be able to remove products in the shopping cart.
 app.delete('/TechShop/deleteAllCart/', (req, res) => {
     let allCartItems = req.params.id;
-    console.log('Cart deleted succesfully...');
+    console.log('At Cart delete endpoint....');
 //    allCartItems = parseInt(allCartItems);
     database.get('Cart').remove(allCartItems).write();
     res.send(allCartItems);
 });
 
 
-//Download all products (then below on what a product should contain)
+//------------------------------------------- GET: get all of the products ------------------------------------------- */
 //http://localhost:5000/TechShop/getProducts
 app.get('/TechShop/getproducts', (req, res) => {
-    let allProducts = getProducts();
+    let allProducts = func.getProducts();
     res.send(JSON.stringify(allProducts));
     
-    console.log('Requests recieved to get all products..');
+    console.log('At get Products end point....');
 });
 
-//Download the shopping cart with all the added products.
+//-------------------------------------------GET: get all Cart items ------------------------------------------- */
 //http://localhost:5000/TechShop/getCartItems
 app.get('/TechShop/getCartItems', (req, res) => {
-    let allCartItems = getCartItems();
+    let allCartItems = func.getCartItems();
     res.send(JSON.stringify(allCartItems));
     
-    console.log('Requests recieved to get all cart items..');
+    console.log('At get Cart Items endpoint....');
 });
 
 app.listen(5000, () => {
